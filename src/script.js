cd0d606a4b38f1f702c0c27ea6de15a87cdfc62d
CynthiaMugo <cynthiamugo25@gmail.com>
2025-04-17 22:08:59 +0300

    Declare the akan names in js

--- src/script.js
+++ src/script.js
@@ -1,2 +1,7 @@
 const form = document.getElementById('form');
 const result = document.getElementById('result');
+
+const akanNames = {
+    male: ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"],
+    female: ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"],
+}
